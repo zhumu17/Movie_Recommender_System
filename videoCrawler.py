0ecf62fd3590b3c7b4ccc63a3d8c0afe9013db48
MuZhu <zhumu17@gmail.com>
2018-02-20 19:34:18 -0500

    added watching trailer function

---
+++ videoCrawler.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def getVideoId(itemName):
+
+    query = itemName
+    url = "https://www.youtube.com/results?search_query=" + query + "trailer"
+
+    resp = requests.get(url)
+    html_data = resp.text
+    soup = BeautifulSoup(html_data, 'html.parser')
+    numScrape = 3
+    tags = soup.find_all("a", attrs={'class':'yt-uix-tile-link'}, limit=numScrape)
+    # print(tags)
+
+
+    for tag in tags:
+        print('TAG:', tag)
+        print('URL:', tag.get('href', None))
+
+    videoURL = tags[0].get('href', None)
+
+    # videoURL="/watch?v=yHfLyMAHrQE"
+
+    print(videoURL)
+    print(type(videoURL ))
+    videoId = re.findall('v\=(.+)', videoURL)
+    if not videoId:
+        videoId = "iG_KZxLHozI" # welcome movie company intros
+    else:
+        videoId = videoId[0]
+
+    return videoId
+
+def getVideoURL(videoId):
+    videoURL = "https://www.youtube.com/embed/" + videoId + "?rel=0&amp;showinfo=0&amp;autoplay=1&amp;mute=1&amp;"
+    return videoURL
