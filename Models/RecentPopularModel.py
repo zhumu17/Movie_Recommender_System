a2980c4d53f8f73901a3182c88cdb867c605b8b2
MuZhu <zhumu17@gmail.com>
2018-03-03 16:31:21 -0500

    check

--- Models/RecentPopularModel.py
+++ Models/RecentPopularModel.py
@@ -1,6 +1,7 @@
 # most popular model
 # here it is a simple design: find the one with highest score with most of the users
 import DatabaseQueries
+import logging
 class RecentPopularModel(object):
     # the year and number of rating thresholds can be modified inside DatabaseQueries SQL query
     def __init__(self):
@@ -8,7 +9,9 @@ class RecentPopularModel(object):
 
     def train(self):
         df_recentPopular = DatabaseQueries.getRecentPopularItem()
-        # print(df_recentPopular)
+        print(df_recentPopular.head())
+        log = logging.getLogger(__name__)
+        log.info("responding to request: %s" % df_recentPopular.head())
         self.recentPopularList = df_recentPopular.itemId.tolist()
 
     # unlike other models, most popular model doesn't need to predict, just directly recommend
